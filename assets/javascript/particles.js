fa26690de221af476958578f13bd13985ed50d73
Bianca Herbst <b.herbst@posteo.de>
2020-08-10 23:11:34 +0200

    define color variables, change buttons, more structuring

--- assets/javascript/particles.js
+++ assets/javascript/particles.js
@@ -1,3 +1,7 @@
+particlesJS.load("particles-js", "particles.json", function () {
+  console.log("particles.js loaded - callback");
+});
+
 particlesJS("particles-js", {
   particles: {
     number: { value: 346, density: { enable: true, value_area: 800 } },
